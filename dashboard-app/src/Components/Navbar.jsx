3517915c8b80a4fd1848047798258b713818264c
Dawood <Dawoodriaz1999@gmail.com>
2023-10-18 16:16:24 +0300

    Created Pages, Controller, Middleware

---
+++ dashboard-app/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = ()=>{
+  return (
+<nav className="Navbar">
+<a href="/home/">Home</a> |
+<a href="/products/">Products</a> |
+<a href="/login/">Login</a> |
+<a href="/register/">Register</a>
+</nav>
+  )
+}
+
+export default Navbar
