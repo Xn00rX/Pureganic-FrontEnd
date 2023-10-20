0f8472518cf34263c242f9f82d139d216621b9a6
n00r <noor.alawadhi999@gmail.com>
2023-10-20 11:32:25 +0300

    productlist

--- dashboard-app/src/Pages/Home.jsx
+++ dashboard-app/src/Pages/Home.jsx
@@ -1,13 +1,14 @@
 import AddProduct from "../Components/AddProduct"
 import Productlist from "../Pages/Productlist"
-
+import Cart from "../Pages/Cart"
 const Home = () => {
   return (
     <div className="Home">
       {/* <AddProduct/>
   <button> Login </button>
   <button> Sign Up </button> */}
-      <Productlist />
+      {/* <Productlist /> */}
+      <Cart />
     </div>
   )
 }
