535d1c208a3494ca6e8e4ecd872d3a5a20e7f2ca
Amira Zahid <[amira.web.media@gmail.com]>
2023-10-25 21:57:04 +0300

    styled some pages

--- dashboard-app/src/Pages/Home.jsx
+++ dashboard-app/src/Pages/Home.jsx
@@ -3,12 +3,25 @@ import Productlist from '../Pages/Productlist'
 import Cart from '../Pages/Cart'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import SideBar from '../images/sidebar.jpg'
+
 import '../App.css'
 
 const Home = () => {
+  const imageStyle = {
+    width: '100%',
+    height: '100%'
+  }
   return (
     <div>
-      <div className="Home"></div>
+      <div className="Home">
+        <p>
+          we create your perfect <span>organic world</span>
+        </p>
+      </div>
+      <div>
+        <img src={SideBar} style={imageStyle} />
+      </div>
     </div>
   )
 }
